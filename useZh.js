584924f3cd14d2b4f629d1cd8db91f2591203691
zherong.czr <zherong.czr@alibaba-inc.com>
2017-04-15 14:44:44 +0800

    feature : use jianFanZh.js to transform chinese

---
+++ useZh.js
@@ -0,0 +1,3 @@
+var jianFanZh = require('./jianFanZh.js');
+var traditioinalStr="雖然這些 repository 放在一起很棒，但它們不一定是最好的學習工具。 像我的情況，我試著了解發生了那些事情，但還是很困惑，所以我決定不從這些大量而且分散的資源來理解。";
+console.log(jianFanZh.simplized(traditioinalStr));
